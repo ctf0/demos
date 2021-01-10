25fe3d0c942c4643a3d431c448b160709f9d829d
ctf0 <muah003@gmail.com>
2021-01-11 01:43:00 +0200

    new mix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,7 @@
 
 // require('./bootstrap');
 
-window.Vue = require('vue')
+import Vue from 'vue'
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,11 +2,6 @@ const mix = require('laravel-mix')
 const webpack = require('webpack')
 
 mix.webpackConfig({
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.js'
-        }
-    },
     plugins: [
         new webpack.ProvidePlugin({
             process : 'process/browser',
